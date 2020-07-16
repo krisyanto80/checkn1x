46cefafcb85007fc91c0c417fcb8cba3948be095
raspberryenvoie <61289832+raspberryenvoie@users.noreply.github.com>
2020-07-16 17:43:50 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ cp /etc/resolv.conf work/chroot/etc
 [ $ARCH = "i686" ] && _ARCH="686" # debian's 32-bit kernels are suffixed "-686"
 cat << EOF | chroot work/chroot /bin/bash
 export DEBIAN_FRONTEND=noninteractive
-apt install -y --no-install-recommends linux-image-$_ARCH live-boot usbmuxd libusbmuxd-tools console-data console-setup console-locales keyboard-configuration
+apt install -y --no-install-recommends linux-image-$_ARCH live-boot usbmuxd libusbmuxd-tools console-data console-setup keyboard-configuration
 sed -i 's/COMPRESS=gzip/COMPRESS=xz/' /etc/initramfs-tools/initramfs.conf
 update-initramfs -u
 rm -f /etc/mtab
